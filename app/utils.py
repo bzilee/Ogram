3f5709bbee87461c15ec37e50fa398c582c698cd
sanix-darker <s4nixd@gmail.com>
2020-04-10 02:26:55 +0100

    Splitting methods when trying to send file on utils.py

--- app/utils.py
+++ app/utils.py
@@ -3,9 +3,29 @@ import json
 from app.settings import *
 
 
-def send_file(chat_id, file_name):
+def get_direct_link(file_id):
     """
-        This method will send a file to the chat_id specified
+
+    :param file_id:
+    :return:
+    """
+    print("[+] Fetching the direct_link the file")
+
+    # Now we fetch the tempory file path
+    url2 = "https://api.telegram.org/bot" + TOKEN + "/getFile?file_id=" + file_id
+    r2 = requests.get(url2)
+    result2 = json.loads(r2.content.decode())
+
+    return "https://api.telegram.org/file/bot" + TOKEN + "/" + result2["result"]["file_path"]
+
+
+def upload_chunk(chat_id, file_name):
+    """
+    This method will build the payload to be upload on telegram api
+
+    :param chat_id:
+    :param file_name:
+    :return:
     """
     print("[+] Sending the file")
     url = "https://api.telegram.org/bot" + TOKEN + "/sendDocument"
@@ -17,32 +37,41 @@ def send_file(chat_id, file_name):
     }
 
     r = requests.post(url, files=files, data=values)
-    result = json.loads(r.content.decode())
-    direct_link = "[x] Operation failed !"
-    print("[+] status : ", result["ok"])
 
-    try:
-        file_name = result["result"]["document"]["file_name"]
+    return json.loads(r.content.decode())
+
+
+def send_chunk(chat_id, file_name):
+    """
+        This method will send a file to the chat_id specified
+    """
+    print("[+] Sending chunk : ", file_name)
+
+    # We upload the chunk and get the result as dict
+    result = upload_chunk(chat_id, file_name)
+
+    if result["ok"]:
         file_id = result["result"]["document"]["file_id"]
-        file_size = result["result"]["document"]["file_size"]
-
-        # thumb = result["result"]["document"]["thumb"]
-
         print("[+] file_id : ", file_id)
-        print("[+] file : ", result["result"]["document"])
 
-        print("[+] Fetching the direct_link the file")
-
-        # Now we fetch the tempory file path
-        url2 = "https://api.telegram.org/bot" + TOKEN + "/getFile?file_id=" + file_id
-        r2 = requests.get(url2)
-        result2 = json.loads(r2.content.decode())
-
-        direct_link = "https://api.telegram.org/file/bot" + TOKEN + "/" + result2["result"]["file_path"]
-    except Exception as es:
-        print("[x] Failed : ", es)
+        # Now we fetch the tempory file path from file_id
+        direct_link = get_direct_link(file_id)
+    else:
+        direct_link = "[x] Error, Operation failed !"
 
     return direct_link
 
 
-# print("direct_link : ", send_file("267092256", "/home/d4rk3r/Pictures/Screenshot from 2020-03-14 18-30-02 - 1.png"))
+# print("direct_link : ", send_chunk("267092256", "/home/d4rk3r/Pictures/Screenshot from 2020-03-14 18-30-02 - 1.png"))
+
+def send_file(file_name):
+    """
+
+    :param file_name:
+    :return:
+    """
+
+    # We split the file First
+
+    # We send the chunks to Telegram
+    pass
